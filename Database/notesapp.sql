1436642a42c6c806017b3795540f4c8be66abe0c
kingjohnpinto <official.johnpinto@gmail.com>
2022-02-08 00:38:11 +0530

    updated sql

--- Database/notesapp.sql
+++ Database/notesapp.sql
@@ -10,6 +10,9 @@ CREATE TABLE notes(
 	note_id INT auto_increment primary key not null,
 	note_name varchar(100) not null,
 	note_contents text ,
+	note_type varchar(50) not null,
+	book_id int not null,
 	created_at datetime default current_timestamp,
 	updated_at datetime default current_timestamp
+	FOREIGN KEY (book_id) REFERENCES notebooks(notebook_id)
 );
