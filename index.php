b9deec5e64115ea198fd4b29ab4a38e77c7e79f0
kingjohnpinto <official.johnpinto@gmail.com>
2022-02-07 14:48:03 +0530

    added navbar

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 include 'includes/header.php';
 include 'includes/footer.php';
+include 'includes/navbar.php';
 ?>
 <?php main_header('Notesapp'); ?>
 
-
+<?php navbar(); ?>
 
 <?php main_footer(); ?>
